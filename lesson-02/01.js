67e4c3c3e15624f9d221acf73c03bd77058a983f
Marcus777-front <markovyorok9777@gmail.com>
2024-11-26 14:37:15 +0300

    lesson 2(03)

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -13,5 +13,5 @@ const isVerifiedUser = true
 const hasSpecialPermission = true
 const hasTemporaryPass = false
 
-let isAccess = (isAdmin || isVerifiedUser) && (hasSpecialPermission || hasSpecialPermission) 
+let isAccess = (isAdmin || isVerifiedUser) && (hasSpecialPermission || hasTemporaryPass) 
 console.log(isAccess);
