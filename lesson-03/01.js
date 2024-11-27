4344a6294f28594fa152a1028489747bb56bcc59
Marcus777-front <markovyorok9777@gmail.com>
2024-11-27 23:17:22 +0300

    lesson 03

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,3 +1,6 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
-function calculateAverage() {}
+function calculateAverage(sum1, sum2) {
+    return (sum1 + sum2) / 2
+}
+
--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,12 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(sum1, sum2, sum3) {
+    if(sum1 > sum2 && sum1 > sum3){
+        return sum1
+    } if (sum2 > sum1 && sum2 > sum3){
+        return sum2
+    } else {
+        return sum3
+    }    
+}
+
